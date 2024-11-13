4bbee85f32884e7449634a5b2538cbf30d840e47
DhimanthShetty <dhimanthshetty19@gmail.com>
2024-11-13 11:40:08 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include<stdbool.h>
+
+int main()
+{
+    /*printf("how many dogs do u have: ");
+    double dogs;
+    scanf("%lf", &dogs);
+    printf("%f\n%e\n%g\n",dogs,dogs,dogs);
+    return 0;
+    int a=3.455;
+    printf("%f",a);
+    char a='A';
+    a++;
+    printf("%c",a);
+    int num;
+    printf("Enter the integer 0-127: ");
+    scanf("%i",&num);
+    printf("%c",num);
+    char a='A'+'\t';
+    printf("A(65)+\\t(11)=%c(%i)",a,a);
+    bool letIsFt=true;
+    printf("let is 1 or 0 :%i\n",letIsFt);
+    printf("%i\n",letIsFt + 5);
+    int x= 2+3*4/3-2;
+    printf("%i",x);
+
+    int burger=5;
+    printf("%i\n",burger);
+    burger+=10;
+    printf("%i",burger);
+    int a = 5, b = 3; // a = 0101, b = 0011
+    printf("%d\n", a & b); // 1  (0001)
+    printf("%d\n", a | b); // 7  (0111)
+    printf("%d\n", a ^ b); // 6  (0110)
+    printf("%d\n", ~a);    // -6 (two's complement)
+    printf("%d\n", a << 1); // 10 (1010)
+    printf("%d\n", a >> 1); // 2  (0010)
+    int a=10;
+    int b=++a;
+    printf("a=%d\n b=%d\n",a,b);*/
+    int a=10, b=2;
+    char d="a";
+    double c=a/b;
+    d=d+3;
+    printf("the result is:%f\n",c);
+    printf("the implisit type casting is:%c",d);
+    return 0;
+
+}
